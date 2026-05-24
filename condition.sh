c557b193cf0d064c191a34cfefee50b5cbab5cdb
siva-0910 <sivaramakrishnaperuboina4@gmail.com>
2026-05-25 00:18:09 +0530

    hello shell

--- condition.sh
+++ condition.sh
@@ -1,6 +1,6 @@
 value=4
 if [ $value -gt 10 ]
-    echo: " $value is grether than 10"
+    echo " $value is grether than 10"
 else    
-    echo: "$value is lesser than 10"
+    echo "$value is lesser than 10"
 fi
